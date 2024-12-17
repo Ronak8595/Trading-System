73771d173a7637744e6387787cc384e0b3cdba7a
Ronak Ramuka <ronak.ramuka@gmail.com>
2024-12-18 02:34:45 +0530

    Minor updates

--- frontend/src/app/order-placement/page.tsx
+++ frontend/src/app/order-placement/page.tsx
@@ -1,112 +1,126 @@
+"use client";
+import React, { useEffect, useState } from "react";
 import { Dialog } from "@/components/Dialog";
 import {
-	Table,
-	TableBody,
-	TableCaption,
-	TableCell,
-	TableHead,
-	TableHeader,
-	TableRow,
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
 } from "@/components/ui/table";
 
-const invoices = [
-	{
-		invoice: "INV001",
-		tradePair: "BTC-USD",
-		quantity: "0.279",
-		expiryTime: "17-12-2024 23:09",
-		status: "Pending",
-		totalAmount: "$250.00",
-	},
-	{
-		invoice: "INV002",
-		tradePair: "BTC-USD",
-		quantity: "0.279",
-		expiryTime: "17-12-2024 23:09",
-		status: "Pending",
-		totalAmount: "$150.00",
-	},
-	{
-		invoice: "INV003",
-		tradePair: "BTC-USD",
-		quantity: "0.279",
-		expiryTime: "17-12-2024 23:09",
-		status: "Pending",
-		totalAmount: "$350.00",
-	},
-];
+interface Order {
+  id: number;
+  userId: number;
+  tokenPair: string;
+  price: number;
+  expiryDate: string;
+  orderType: string;
+  quantity: number;
+  settledQuantity: number;
+  createdAt: string;
+  updatedAt: string;
+}
 
 const OrderPlacement = () => {
-	return (
-		<div className="h-full w-full">
-			<div className="w-[80%] mx-auto py-10 ">
-				<h1 className="text-center text-2xl text-blue-500">
-					User DashBoard
-				</h1>
-				<div className="flex justify-between mt-8 sm:flex-row flex-col gap-6 items-center sm:items-start">
-					<div className="p-4 bg-white/10 rounded-lg shadow-md w-full sm:w-1/3">
-						<h2 className="text-center text-xl text-blue-500 mb-4">
-							Total Balance
-						</h2>
-						<div className="w-full flex justify-between items-center flex-col gap-4">
-							<div className="flex justify-between w-full">
-								<h3>USD</h3> <h3>$1000.00</h3> <h3>BUY</h3>{" "}
-								<h3>SWAP</h3>
-							</div>
-							<div className="flex justify-between w-full">
-								<h3>BTC</h3> <h3>$9900.00</h3> <h3>BUY</h3>{" "}
-								<h3>SWAP</h3>
-							</div>
-							<div className="flex justify-between w-full">
-								<h3>ETH</h3> <h3>$2000.00</h3> <h3>BUY</h3>{" "}
-								<h3>SWAP</h3>
-							</div>
-						</div>
-					</div>
-					<Dialog />
-				</div>
-				{/* order history */}
-				<div className="bg-white/10 rounded-lg shadow-md p-6 mt-6 sm:mt-10">
-					<h3 className="text-center text-xl text-blue-500 mb-4">
-						Order History
-					</h3>
-					<Table>
-						<TableCaption>
-							A brief history of your recent orders.
-						</TableCaption>
-						<TableHeader>
-							<TableRow>
-								<TableHead className="w-[100px]">Id</TableHead>
-								<TableHead>Trade Pair</TableHead>
-								<TableHead>Quantity</TableHead>
-								<TableHead>Expiry Time</TableHead>
-								<TableHead>Status</TableHead>
-								<TableHead className="text-right">
-									Amount
-								</TableHead>
-							</TableRow>
-						</TableHeader>
-						<TableBody>
-							{invoices.map((invoice) => (
-								<TableRow key={invoice.invoice}>
-									<TableCell className="font-medium">
-										{invoice.invoice}
-									</TableCell>
-									<TableCell>{invoice.tradePair}</TableCell>
-									<TableCell>{invoice.quantity}</TableCell>
-									<TableCell>{invoice.expiryTime}</TableCell>
-									<TableCell>{invoice.status}</TableCell>
-									<TableCell className="text-right">
-										{invoice.totalAmount}
-									</TableCell>
-								</TableRow>
-							))}
-						</TableBody>
-					</Table>
-				</div>
-			</div>
-		</div>
-	);
+  const [orders, setOrders] = useState<Order[]>([]);
+  const userId = 1;
+
+  const fetchOrders = async () => {
+    try {
+      const response = await fetch(`/users/${userId}/orders`);
+      if (!response.ok) {
+        throw new Error("Failed to fetch orders");
+      }
+      const data: Order[] = await response.json();
+      setOrders(data);
+    } catch (error) {
+      console.error("Error fetching orders:", error);
+    }
+  };
+
+  const getStatus = (quantity: number, settledQuantity: number): string => {
+    if (settledQuantity === quantity) return "COMPLETED";
+    if (settledQuantity !== 0) return "PARTIAL";
+    return "IN PROCESS";
+  };
+
+  useEffect(() => {
+    fetchOrders();
+  }, []);
+
+  return (
+    <div className="h-full w-full">
+      <div className="w-[80%] mx-auto py-10">
+        <h1 className="text-center text-2xl text-blue-500">User Dashboard</h1>
+        <div className="flex justify-between mt-8 sm:flex-row flex-col gap-6 items-center sm:items-start">
+          <div className="p-4 bg-white/10 rounded-lg shadow-md w-full sm:w-1/3">
+            <h2 className="text-center text-xl text-blue-500 mb-4">
+              Total Balance
+            </h2>
+            <div className="w-full flex justify-between items-center flex-col gap-4">
+              <div className="flex justify-between w-full">
+                <h3>USD</h3> <h3>$1000.00</h3> <h3>BUY</h3> <h3>SWAP</h3>
+              </div>
+              <div className="flex justify-between w-full">
+                <h3>BTC</h3> <h3>$9900.00</h3> <h3>BUY</h3> <h3>SWAP</h3>
+              </div>
+              <div className="flex justify-between w-full">
+                <h3>ETH</h3> <h3>$2000.00</h3> <h3>BUY</h3> <h3>SWAP</h3>
+              </div>
+            </div>
+          </div>
+          <Dialog />
+        </div>
+
+        {/* Order History */}
+        <div className="bg-white/10 rounded-lg shadow-md p-6 mt-6 sm:mt-10">
+          <h3 className="text-center text-xl text-blue-500 mb-4">
+            Order History
+          </h3>
+          <Table>
+            <TableCaption>A brief history of your recent orders.</TableCaption>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="w-[100px]">Id</TableHead>
+                <TableHead>Token Pair</TableHead>
+                <TableHead>Quantity</TableHead>
+                <TableHead>Expiry Time</TableHead>
+                <TableHead>Status</TableHead>
+                <TableHead className="text-right">Price</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {orders.length > 0 ? (
+                orders.map((order) => (
+                  <TableRow key={order.id}>
+                    <TableCell className="font-medium">{order.id}</TableCell>
+                    <TableCell>{order.tokenPair}</TableCell>
+                    <TableCell>{order.quantity}</TableCell>
+                    <TableCell>
+                      {new Date(order.expiryDate).toLocaleString()}
+                    </TableCell>
+                    <TableCell>
+                      {getStatus(order.quantity, order.settledQuantity)}
+                    </TableCell>
+                    <TableCell className="text-right">${order.price}</TableCell>
+                  </TableRow>
+                ))
+              ) : (
+                <TableRow>
+                  <TableCell colSpan={6} className="text-center">
+                    No orders found.
+                  </TableCell>
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default OrderPlacement;
--- frontend/src/components/Dialog.tsx
+++ frontend/src/components/Dialog.tsx
@@ -7,294 +7,255 @@ import { CalendarIcon } from "lucide-react";
 import { cn } from "@/lib/utils";
 import { Calendar } from "@/components/ui/calendar";
 import {
-	Popover,
-	PopoverContent,
-	PopoverTrigger,
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
 } from "@/components/ui/popover";
 import { Button } from "@/components/ui/button";
 import {
-	Dialog as DialogWrapper,
-	DialogContent,
-	DialogDescription,
-	DialogFooter,
-	DialogHeader,
-	DialogTitle,
-	DialogTrigger,
+  Dialog as DialogWrapper,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import {
-	Select,
-	SelectContent,
-	SelectGroup,
-	SelectItem,
-	SelectLabel,
-	SelectTrigger,
-	SelectValue,
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
 } from "@/components/ui/select";
 import { useForm, SubmitHandler } from "react-hook-form";
 
 interface FormData {
-	asset: string | undefined;
-	quantity: string;
-	price: number | undefined;
-	expirationOption: string | undefined;
-	durationValue: string | undefined;
-	durationUnit: string | undefined;
-	expirationDate: Date | undefined;
+  asset: string | undefined;
+  quantity: string;
+  price: number | undefined;
+  expirationOption: string | undefined;
+  durationValue: string | undefined;
+  durationUnit: string | undefined;
+  expirationDate: Date | undefined;
 }
 
 interface ConversionRate {
-	symbol: string;
-	price: string;
+  symbol: string;
+  price: string;
 }
 
 export function Dialog() {
-	const [conversionRate, setConversionRate] = useState<ConversionRate[]>([]);
-	const [price, setPrice] = useState<number | undefined>(undefined);
+  const [conversionRate, setConversionRate] = useState<ConversionRate[]>([]);
+  const [price, setPrice] = useState<number | undefined>(undefined);
 
-	const { register, handleSubmit, setValue, watch } = useForm<FormData>({
-		defaultValues: {
-			asset: undefined,
-			quantity: "",
-			price: undefined,
-			expirationOption: "duration",
-			durationValue: undefined,
-			durationUnit: "minutes",
-			expirationDate: undefined,
-		},
-	});
+  const { register, handleSubmit, setValue, watch } = useForm<FormData>({
+    defaultValues: {
+      asset: undefined,
+      quantity: "",
+      price: undefined,
+      expirationOption: "duration",
+      durationValue: undefined,
+      durationUnit: "minutes",
+      expirationDate: undefined,
+    },
+  });
 
-	const [date, setDate] = React.useState<Date | undefined>(undefined);
+  const [date, setDate] = React.useState<Date | undefined>(undefined);
 
-	const watchedExpirationOption = watch("expirationOption");
-	const watchedExpirationDate = watch("expirationDate");
+  const watchedExpirationOption = watch("expirationOption");
+  const watchedExpirationDate = watch("expirationDate");
 
-	const handleDateChange = (selectedDate: Date | undefined) => {
-		setDate(selectedDate);
-		setValue("expirationDate", selectedDate);
-	};
+  const handleDateChange = (selectedDate: Date | undefined) => {
+    setDate(selectedDate);
+    setValue("expirationDate", selectedDate);
+  };
 
-	const onSubmit: SubmitHandler<FormData> = (data) => {
-		setPrice(data.price);
-		console.log("Form Data on Submit:", data);
-	};
+  const onSubmit: SubmitHandler<FormData> = (data) => {
+    setPrice(data.price);
+    console.log("Form Data on Submit:", data);
+  };
 
-	useEffect(() => {
-		const fetchConversionRate = async () => {
-			const API = "https://api.binance.com/api/v3/ticker/price";
+  useEffect(() => {
+    const fetchConversionRate = async () => {
+      const API = "https://api.binance.com/api/v3/ticker/price";
 
-			try {
-				const res = await fetch(API);
-				const data: ConversionRate[] = await res.json();
+      try {
+        const res = await fetch(API);
+        const data: ConversionRate[] = await res.json();
 
-				setConversionRate((prevState) => {
-					// Stop adding if we already have 10 items
-					if (prevState.length >= 15) return prevState;
+        setConversionRate((prevState) => {
+          // Stop adding if we already have 10 items
+          if (prevState.length >= 15) return prevState;
 
-					// Add new rates to the state and ensure we don't exceed 10 items
-					const newRates = [...prevState, ...data].slice(0, 15);
-					return newRates;
-				});
-			} catch (error) {
-				console.error("Error fetching conversion rates:", error);
-			}
-		};
+          // Add new rates to the state and ensure we don't exceed 10 items
+          const newRates = [...prevState, ...data].slice(0, 15);
+          return newRates;
+        });
+      } catch (error) {
+        console.error("Error fetching conversion rates:", error);
+      }
+    };
 
-		// Set interval for 5 seconds
-		const intervalId = setInterval(fetchConversionRate, 5000);
+    // Set interval for 5 seconds
+    const intervalId = setInterval(fetchConversionRate, 5000);
 
-		// Cleanup interval on unmount
-		return () => clearInterval(intervalId);
-	}, []);
+    // Cleanup interval on unmount
+    return () => clearInterval(intervalId);
+  }, []);
 
-	return (
-		<div>
-			<DialogWrapper>
-				<DialogTrigger asChild>
-					<Button variant="outline">Create Order</Button>
-				</DialogTrigger>
-				<DialogContent className="sm:max-w-[425px]">
-					<form onSubmit={handleSubmit(onSubmit)}>
-						<DialogHeader>
-							<DialogTitle>Create Order</DialogTitle>
-							<DialogDescription>
-								Add your order details here.
-							</DialogDescription>
-						</DialogHeader>
-						<div className="grid gap-4 py-4">
-							{/* Asset Selector */}
-							<div className="grid grid-cols-4 items-center gap-4">
-								<Select
-									onValueChange={(value) =>
-										setValue("asset", value)
-									}
-								>
-									<SelectTrigger className="w-[200px]">
-										<SelectValue placeholder="Asset selector" />
-									</SelectTrigger>
-									<SelectContent>
-										<SelectGroup>
-											{conversionRate.map((rate) => (
-												<SelectItem
-													value={
-														rate.symbol +
-														" : " +
-														rate.price
-													}
-													className="flex justify-between items-center gap-6 "
-												>
-													<h5>{rate.symbol}</h5>
-													<h5 className="text-blue-600">
-														{rate.price}
-													</h5>
-												</SelectItem>
-											))}
-										</SelectGroup>
-									</SelectContent>
-								</Select>
-							</div>
+  return (
+    <div>
+      <DialogWrapper>
+        <DialogTrigger asChild>
+          <Button variant="outline">Create Order</Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[425px]">
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <DialogHeader>
+              <DialogTitle>Create Order</DialogTitle>
+              <DialogDescription>
+                Add your order details here.
+              </DialogDescription>
+            </DialogHeader>
+            <div className="grid gap-4 py-4">
+              {/* Asset Selector */}
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Select onValueChange={(value) => setValue("asset", value)}>
+                  <SelectTrigger className="w-[200px]">
+                    <SelectValue placeholder="Asset selector" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectGroup>
+                      {conversionRate.map((rate) => (
+                        <SelectItem
+                          key={rate.symbol}
+                          value={rate.symbol + " : " + rate.price}
+                          className="flex justify-between items-center gap-6 "
+                        >
+                          <h5>{rate.symbol}</h5>
+                          <h5 className="text-blue-600">{rate.price}</h5>
+                        </SelectItem>
+                      ))}
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+              </div>
 
-							{/* Quantity Input */}
-							<div className="grid grid-cols-4 items-center gap-4">
-								<Label
-									htmlFor="quantity"
-									className="text-right"
-								>
-									Quantity								</Label>
-								<Input
-									id="quantity"
-									className="col-span-3"
-									{...register("quantity")}
-								/>
-							</div>
+              {/* Quantity Input */}
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="quantity" className="text-right">
+                  Quantity{" "}
+                </Label>
+                <Input
+                  id="quantity"
+                  className="col-span-3"
+                  {...register("quantity")}
+                />
+              </div>
 
-							{/* Price Input */}
-							<div className="grid grid-cols-4 items-center gap-4">
-								<Select
-									onValueChange={(value) =>
-										setValue("asset", value)
-									}
-								>
-									<SelectTrigger className="w-[200px]">
-										<SelectValue placeholder="BUY/SELL" />
-									</SelectTrigger>
-									<SelectContent>
-										<SelectGroup></SelectGroup>
-										<SelectItem value="BUY">BUY</SelectItem>
-										<SelectItem value="SELL">SELL</SelectItem>
-									</SelectContent>
-								</Select>
-							</div>
+              {/* Price Input */}
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Select onValueChange={(value) => setValue("asset", value)}>
+                  <SelectTrigger className="w-[200px]">
+                    <SelectValue placeholder="BUY/SELL" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectGroup></SelectGroup>
+                    <SelectItem value="BUY">BUY</SelectItem>
+                    <SelectItem value="SELL">SELL</SelectItem>
+                  </SelectContent>
+                </Select>
+              </div>
 
-							{/* Expiration Option */}
-							<div className="grid grid-cols-4 items-center gap-4">
-								<Label
-									htmlFor="expirationOption"
-									className="text-right"
-								>
-									Expiration
-								</Label>
-								<Select
-									value={watchedExpirationOption ?? undefined}
-									onValueChange={(value) =>
-										setValue("expirationOption", value)
-									}
-								>
-									<SelectTrigger className="col-span-3">
-										<SelectValue placeholder="Choose expiration type" />
-									</SelectTrigger>
-									<SelectContent>
-										<SelectGroup>
-											<SelectItem value="duration">
-												Time Duration
-											</SelectItem>
-											<SelectItem value="specific">
-												Specific Date
-											</SelectItem>
-										</SelectGroup>
-									</SelectContent>
-								</Select>
-							</div>
+              {/* Expiration Option */}
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="expirationOption" className="text-right">
+                  Expiration
+                </Label>
+                <Select
+                  value={watchedExpirationOption ?? undefined}
+                  onValueChange={(value) => setValue("expirationOption", value)}
+                >
+                  <SelectTrigger className="col-span-3">
+                    <SelectValue placeholder="Choose expiration type" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectGroup>
+                      <SelectItem value="duration">Time Duration</SelectItem>
+                      <SelectItem value="specific">Specific Date</SelectItem>
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+              </div>
 
-							{/* Time Duration Inputs */}
-							{watchedExpirationOption === "duration" && (
-								<div className="grid grid-cols-4 items-center gap-4">
-									<Input
-										id="durationValue"
-										className="col-span-2"
-										placeholder="Value"
-										type="number"
-										{...register("durationValue")}
-									/>
-									<Select
-										value={watch("durationUnit")}
-										onValueChange={(value) =>
-											setValue("durationUnit", value)
-										}
-									>
-										<SelectTrigger>
-											<SelectValue placeholder="Unit" />
-										</SelectTrigger>
-										<SelectContent>
-											<SelectGroup>
-												<SelectItem value="seconds">
-													Seconds
-												</SelectItem>
-												<SelectItem value="minutes">
-													Minutes
-												</SelectItem>
-												<SelectItem value="hours">
-													Hours
-												</SelectItem>
-												<SelectItem value="days">
-													Days
-												</SelectItem>
-												<SelectItem value="weeks">
-													Weeks
-												</SelectItem>
-											</SelectGroup>
-										</SelectContent>
-									</Select>
-								</div>
-							)}
+              {/* Time Duration Inputs */}
+              {watchedExpirationOption === "duration" && (
+                <div className="grid grid-cols-4 items-center gap-4">
+                  <Input
+                    id="durationValue"
+                    className="col-span-2"
+                    placeholder="Value"
+                    type="number"
+                    {...register("durationValue")}
+                  />
+                  <Select
+                    value={watch("durationUnit")}
+                    onValueChange={(value) => setValue("durationUnit", value)}
+                  >
+                    <SelectTrigger>
+                      <SelectValue placeholder="Unit" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectGroup>
+                        <SelectItem value="seconds">Seconds</SelectItem>
+                        <SelectItem value="minutes">Minutes</SelectItem>
+                        <SelectItem value="hours">Hours</SelectItem>
+                        <SelectItem value="days">Days</SelectItem>
+                        <SelectItem value="weeks">Weeks</SelectItem>
+                      </SelectGroup>
+                    </SelectContent>
+                  </Select>
+                </div>
+              )}
 
-							{/* Specific Date Picker */}
-							{watchedExpirationOption === "specific" && (
-								<div className="grid grid-cols-4 items-center gap-4">
-									<Popover>
-										<PopoverTrigger asChild>
-											<Button
-												variant={"outline"}
-												className={cn(
-													"w-[280px] justify-start text-left font-normal",
-													!date &&
-														"text-muted-foreground"
-												)}
-											>
-												<CalendarIcon className="mr-2 h-4 w-4 z-10" />
-												{date
-													? format(date, "PPP")
-													: "Pick a date"}
-											</Button>
-										</PopoverTrigger>
-										<PopoverContent className="w-auto p-0 bg-blue-700 z-50 pointer-events-auto">
-											<Calendar
-												mode="single"
-												selected={date}
-												onSelect={handleDateChange}
-											/>
-										</PopoverContent>
-									</Popover>
-								</div>
-							)}
-						</div>
-						<DialogFooter>
-							<Button type="submit">Create</Button>
-						</DialogFooter>
-					</form>
-				</DialogContent>
-			</DialogWrapper>
-		</div>
-	);
+              {/* Specific Date Picker */}
+              {watchedExpirationOption === "specific" && (
+                <div className="grid grid-cols-4 items-center gap-4">
+                  <Popover>
+                    <PopoverTrigger asChild>
+                      <Button
+                        variant={"outline"}
+                        className={cn(
+                          "w-[280px] justify-start text-left font-normal",
+                          !date && "text-muted-foreground"
+                        )}
+                      >
+                        <CalendarIcon className="mr-2 h-4 w-4 z-10" />
+                        {date ? format(date, "PPP") : "Pick a date"}
+                      </Button>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-auto p-0 bg-blue-700 z-50 pointer-events-auto">
+                      <Calendar
+                        mode="single"
+                        selected={date}
+                        onSelect={handleDateChange}
+                      />
+                    </PopoverContent>
+                  </Popover>
+                </div>
+              )}
+            </div>
+            <DialogFooter>
+              <Button type="submit">Create</Button>
+            </DialogFooter>
+          </form>
+        </DialogContent>
+      </DialogWrapper>
+    </div>
+  );
 }
